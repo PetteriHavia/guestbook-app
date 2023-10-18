8a7b0ed023028a717a48633b38d617e6e340ab6a
Petteri Havia <petsku15@hotmail.fi>
2023-10-18 21:25:51 +0300

    Function to show sidebar using toggle state

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,13 +2,17 @@ import { useContext } from "react";
 import { useMessageContext } from "../context/MessageContextProvider";
 
 const Header = () => {
-  const { messages } = useMessageContext();
+  const { messages, toggle, setToggle } = useMessageContext();
+
+  const showSidebar = () => {
+    setToggle(!toggle);
+  }
 
   return (
     <div className="header-container">
       <div>
         <h1>Digital Guestbook</h1>
-        <button>Write</button>
+        <button onClick={showSidebar}>Write</button>
       </div>
       <h3>Messages: {messages && messages.length}</h3>
     </div>
